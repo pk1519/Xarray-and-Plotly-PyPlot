6537d0b6a82f05b39bc39708ea7c415f38a7a305
Priyanshu Kumar <priyanshu345kumar@gmail.com>
2026-02-09 23:08:55 +0530

    Clean up code comments and formatting

--- task1_sfd_bmd.py
+++ task1_sfd_bmd.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 from data.element import members
 from data.node import nodes
 
-# -------------------
-# Load dataset
-# -------------------
+
 ds = xr.open_dataset("data/screening_task.nc")
 
 central_elements = [15,24,33,42,51,60,69,78,83]
@@ -27,7 +25,7 @@ for ele in central_elements:
 
     length = np.linalg.norm(xj - xi)
 
-    # ---- Select force components ----
+   
     Vy_i = ds["forces"].sel(Element=ele, Component="Vy_i").values.item()
     Vy_j = ds["forces"].sel(Element=ele, Component="Vy_j").values.item()
 
@@ -40,9 +38,7 @@ for ele in central_elements:
 
     current_length += length
 
-# -------------------
-# Plot SFD
-# -------------------
+
 plt.figure()
 plt.plot(x_positions, shear)
 plt.fill_between(x_positions, shear, 0, alpha=0.3)
@@ -52,9 +48,7 @@ plt.ylabel("Vy")
 plt.grid()
 plt.show()
 
-# -------------------
-# Plot BMD
-# -------------------
+
 plt.figure()
 plt.plot(x_positions, moment)
 plt.fill_between(x_positions, moment, 0, alpha=0.3)
